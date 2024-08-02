69684aa25ef25ab22722e044977e29a3fa827622
Bekmurod G'ofurov <bekmurod@kali>
2024-08-02 18:01:09 +0500

    2-lesson

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ from asyncio import run
 
 dp = Dispatcher()
 
+async def start_up_navigation(bot: Bot):
+    await bot.send_message(5841656536, "Bot ishga tushdi✅")
+
+async def shutdwon_navigation(bot: Bot):
+    await bot.send_message(5841656536, "Bot ishdan to'xtadi❕")    
+
 async  def echo(message: types.Message, bot: Bot):
     await message.copy_to(chat_id=message.chat.id)
 
 async def start():
+    dp.startup.register(start_up_navigation)
     dp.message.register(echo)
+    dp.shutdown.register(shutdwon_navigation)
 
     bot = Bot("5826284002:AAH8Hld-eqgotaxlmL7-QImA14vtyIEhAnY")
     await dp.start_polling(bot)    
-    print("It is polling")
 
 run(start())
