d28e724b5d9969cf5a14944a6ca3a7005bad54f9
Bekmurod G'ofurov <bekmurod@kali>
2024-08-06 01:48:21 +0500

    3-lesson fix

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 from aiogram import Bot, Dispatcher, types
+from aiogram.types import BotCommand
+from aiogram.filters import Command
 from asyncio import run
-from functions import get_user_info
+from dotenv import load_dotenv
+from functions import *
+import os
 
 dp = Dispatcher()
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+ADMINS = os.getenv('ADMINS')
 
 async def start_up_navigation(bot: Bot):
-    await bot.send_message(5841656536, "Bot ishga tushdi✅")
+    await bot.send_message(ADMINS, "Bot ishga tushdi✅")
 
 async def shutdwon_navigation(bot: Bot):
-    await bot.send_message(5841656536, "Bot ishdan to'xtadi❕")    
+    await bot.send_message(ADMINS, "Bot ishdan to'xtadi❕")    
 
 # async  def echo(message: types.Message, bot: Bot):
 #     await message.copy_to(chat_id=message.chat.id)
 
 async def start():
     dp.startup.register(start_up_navigation)
+
+    dp.message.register(start_answer, Command("start"))
+    dp.message.register(help_answer, Command("help"))
+
     dp.message.register(get_user_info)
     dp.shutdown.register(shutdwon_navigation)
 
-    bot = Bot("5826284002:AAH8Hld-eqgotaxlmL7-QImA14vtyIEhAnY")
+    bot = Bot(BOT_TOKEN)
+    await bot.set_my_commands([
+        BotCommand(command="/start", description="Botni ishga tushirish" ),
+        BotCommand(command="/help", description="Yordam olish" ),
+    ])
+    
     await dp.start_polling(bot)    
-
+    
 run(start())
