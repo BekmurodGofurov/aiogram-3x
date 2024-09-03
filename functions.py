5e01f53c5f8abee9acae4d455ad047bbdec5d7a2
Bekmurod G'ofurov <bekmurod@kali>
2024-09-03 12:22:47 +0500

    17-lesson Group memberlarni mute, unmute, ban va unban qilish

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-from aiogram.types import Message
+from aiogram.types import Message, ChatPermissions
 
 async def get_info(message: Message):
     await message.answer(f"""
@@ -14,3 +14,25 @@ async def new_chat_members_answer(message: Message):
 async def left_chat_member_answer(message: Message):
     await message.answer(text=F"Yaxshi boring {message.left_chat_member.full_name}")
     await message.delete()  
+
+async def mute_member_answer(message: Message):
+    user_id = message.reply_to_message.from_user.id
+    permission = ChatPermissions(can_send_message=False)
+    await message.chat.restrict(user_id, permission)
+    await message.answer(f"{message.reply_to_message.from_user.mention_html()} yozishdan mahrum qilindi!", parse_mode='HTML')
+
+async def unmute_member_answer(message: Message):
+    user_id = message.reply_to_message.from_user.id
+    permission = ChatPermissions(can_send_message=True,can_invite_users=True,can_send_media_messages=True,can_send_other_messages=False,)
+    await message.chat.restrict(user_id, permission)
+    await message.answer(f"{message.reply_to_message.from_user.mention_html()} endi yoza olsiz!", parse_mode='HTML')
+
+async def ban_member_answer(message: Message):
+    user_id = message.reply_to_message.from_user.id
+    await message.chat.ban_sender_chat(user_id)
+    await message.answer(f"{message.reply_to_message.from_user.mention_html()} Gruhdan haydaldi", parse_mode='HTML')
+
+async def unban_member_answer(message: Message):
+    user_id = message.reply_to_message.from_user.id
+    await message.chat.unban_sender_chat(user_id)
+    await message.answer(f"{message.reply_to_message.from_user.mention_html()} Blockdan ozod qilindi", parse_mode='HTML')
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import functions
 dp=Dispatcher()
 
 async def startup_answer(bot: Bot):
-    await bot.send_message(your id, f"Bot is running")
+    await bot.send_message(your id , f"Bot is running")
 
 async def shutdown_answer(bot: Bot):
     await bot.send_message(your id, f"bot stoped running")
@@ -17,6 +17,11 @@ async def start():
     dp.message.register(functions.new_chat_members_answer, and_f(F.chat.id == -1002233391717, F.new_chat_members))
     dp.message.register(functions.left_chat_member_answer, and_f(F.chat.id == -1002233391717, F.left_chat_member))
     dp.message.register(functions.get_info, F.chat.id != -1002233391717)
+
+    dp.message.register(functions.mute_member_answer, and_f(F.chat.type =="supergroup", and_f(F.text == "/mute", F.reply_to_message)))
+    dp.message.register(functions.unmute_member_answer, and_f(F.chat.type =="supergroup", and_f(F.text == "/unmute", F.reply_to_message)))
+    dp.message.register(functions.ban_member_answer, and_f(F.chat.type =="supergroup", and_f(F.text == "/ban", F.reply_to_message)))
+    dp.message.register(functions.unban_member_answer, and_f(F.chat.type =="supergroup", and_f(F.text == "/unban", F.reply_to_message)))
 
     bot = Bot("Your bot token")
     await dp.start_polling(bot, polling_timeout=1)
